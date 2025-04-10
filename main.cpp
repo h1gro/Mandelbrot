69b37bc88eb09ea55a75a4943d13da5802596b91
h1gro <komarov.aa@phystech.edu>
2025-04-10 23:52:29 +0300

    enums, consts and more clear code

--- main.cpp
+++ main.cpp
@@ -1,85 +1,98 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <math.h>
-
 #include "mandelbrot.h"
 
 int main()
 {
+    struct window_t win_param = {};
+
+    win_param.length = WINDOW_LENGTH;
+    win_param.weigth = WINDOW_WEIGHT;
+
     //init array of pixels and window for picture
-    sf::VertexArray pixels(sf::Points, WINDOW_LENGTH * WINDOW_WEIGHT); //type Points!!!
-    sf::RenderWindow window(sf::VideoMode({WINDOW_LENGTH, WINDOW_WEIGHT}), "Mandelbrot");
-    window.setVerticalSyncEnabled(true);
-    PixelsDefault(pixels);
-    //FillingPixels(pixels);
-    DrawWindow(window, pixels);
+    sf::RenderWindow window(sf::VideoMode({win_param.length, win_param.weigth}), "Mandelbrot");
+    sf::VertexArray pixels(sf::Points, win_param.length * win_param.weigth);
+
+    PixelsDefault(pixels, &win_param);
+
+    DrawWindow(window, pixels, &win_param);
 
     return 0;
 }
 
-void FillingPixels(sf::VertexArray& pixels)
+void FillingPixels(sf::VertexArray& pixels, struct window_t* win_param)
 {
     assert(&pixels);
+    assert(win_param);
 
-    double y0 = 0, x0 = 0;
+    double y_first = 0, x_first = 0;
     int index = 0;
 
-    for (int yi = 0; yi < WINDOW_WEIGHT; yi++)
+    for (int y_curr = 0; y_curr < win_param->weigth; y_curr++)
     {
-        y0 = (yi - Y0) / (WINDOW_WEIGHT / 4.0);
-        for (int xi = 0; xi < WINDOW_LENGTH; xi++)
+        y_first = (y_curr - Y_MIDDLE) / (win_param->weigth / ZOOM_CONST);
+
+        for (int x_curr = 0; x_curr < win_param->length; x_curr++)
         {
-            x0 = (xi - X0) / (WINDOW_LENGTH / 4.0);
+            x_first = (x_curr - X_MIDDLE) / (win_param->length / ZOOM_CONST);
 
-            index = xi + yi * WINDOW_LENGTH;
+            index = x_curr + y_curr * win_param->length;
 
-            MandelbrotCalculation(pixels, x0, y0, xi, yi, index);
+            MandelbrotCalculation(pixels, x_first, y_first, x_curr, y_curr, index);
         }
     }
 }
 
-void MandelbrotCalculation(sf::VertexArray& pixels, double x0, double y0, int xi, int yi, int index)
+void MandelbrotCalculation(sf::VertexArray& pixels, double x_first, double y_first, int x_curr, int y_curr, int index)
 {
     assert(&pixels);
     int calcs = 0;
-    double old_x = 0;
-    double x = x0, y = y0;
+    double x = x_first, y = y_first, old_x = 0;
 
-    while ((x * x + y * y <= 10000) && (calcs < MAX_CALCS))
+    while ((x * x + y * y <= MAX_RADIUS) && (calcs < MAX_CALCS))
     {
         old_x = x;
-        x = x * x - y * y + x0;
-        y = 2 * old_x * y + y0;
+        x = x * x - y * y + x_first;
+        y = 2 * old_x * y + y_first;
         calcs++;
     }
 
-    //int index = xi + yi * WINDOW_LENGTH;
-    pixels[index].position = sf::Vector2f (xi, yi);
-    pixels[index].color    = sf::Color(calcs * 18 % MAX_CALCS, calcs * 10 % MAX_CALCS, calcs * 15 % MAX_CALCS);
+    PointAppropriation(pixels, calcs, x_curr, y_curr, index);
 }
 
-void PixelsDefault(sf::VertexArray& pixels)
+void PointAppropriation(sf::VertexArray& pixels, int calcs, int x_curr, int y_curr, int index)
 {
     assert(&pixels);
 
-    int i = 0;
+    int red   = calcs * RED_COEF   % MAX_CALCS;
+    int green = calcs * GREEN_COEF % MAX_CALCS;
+    int blue  = calcs * BLUE_COEF  % MAX_CALCS;
 
-    for (int y = 0; y < WINDOW_WEIGHT; y++)
+    pixels[index].position = sf::Vector2f (x_curr, y_curr);
+    pixels[index].color    = sf::Color (red, green, blue);
+}
+
+void PixelsDefault(sf::VertexArray& pixels, struct window_t* win_param)
+{
+    assert(&pixels);
+    assert(win_param);
+
+    int index = 0;
+
+    for (int y = 0; y < win_param->weigth; y++)
     {
-        for (int x = 0; x < WINDOW_LENGTH; x++)
+        for (int x = 0; x < win_param->length; x++)
         {
-            i = x + y * WINDOW_LENGTH;
-            pixels[i].position = sf::Vector2f(x, y);
-            pixels[i].color    = sf::Color::Black;
+            index = x + y * win_param->length;
+            pixels[index].position = sf::Vector2f(x, y);
+            pixels[index].color    = sf::Color::Black;
         }
     }
 }
 
-void DrawWindow(sf::RenderWindow& window, sf::VertexArray& pixels)
+void DrawWindow(sf::RenderWindow& window, sf::VertexArray& pixels, struct window_t* win_param)
 {
     assert(&pixels);
     assert(&window);
+    assert(win_param);
 
     while (window.isOpen()) //while window is open
     {
@@ -96,9 +109,9 @@ void DrawWindow(sf::RenderWindow& window, sf::VertexArray& pixels)
             }
         }
 
-        PixelsDefault(pixels);//black all
+        //PixelsDefault(pixels);//black all
 
-        FillingPixels(pixels);
+        FillingPixels(pixels, win_param);
 
         window.clear(); //clear content of the window and draw all window in black
         window.draw(pixels);
--- mandelbrot.h
+++ mandelbrot.h
@@ -3,18 +3,49 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <math.h>
 
-const int WINDOW_LENGTH = 800;
-const int WINDOW_WEIGHT = 600;
+// struct window_t
+// {
+//     sf::VertexArray* pixels;
+//     sf::RenderWindow* window;
+//     int length;
+//     int weight;
+// };
 
-const int X0 = 500;
-const int Y0 = 300;
+struct window_t
+{
+    u_int length;
+    u_int weigth;
+};
+
+enum win_parametres
+{
+    WINDOW_LENGTH = 800,
+    WINDOW_WEIGHT = 800,
+    X_MIDDLE      = 500,
+    Y_MIDDLE      = 400,
+};
+
+enum color
+{
+    RED_COEF   = 18,
+    GREEN_COEF = 10,
+    BLUE_COEF  = 15,
+};
+
 const int MAX_CALCS = 256;
 
-void DrawWindow            (sf::RenderWindow& window, sf::VertexArray& pixels);
-void PixelsDefault         (sf::VertexArray& pixels);
-void FillingPixels         (sf::VertexArray& pixels);
+const double MAX_RADIUS = 10000;
+const double ZOOM_CONST = 4.0;
 
-void MandelbrotCalculation(sf::VertexArray& pixels, double x0, double y0, int xi, int yi, int index);
+void PixelsDefault         (sf::VertexArray& pixels, struct window_t* win_param);
+void FillingPixels         (sf::VertexArray& pixels, struct window_t* win_param);
+void PointAppropriation    (sf::VertexArray& pixels, int calcs, int x_curr, int y_curr, int index);
+void DrawWindow            (sf::RenderWindow& window, sf::VertexArray& pixels, struct window_t* win_param);
+void MandelbrotCalculation (sf::VertexArray& pixels, double x_first, double y_first, int x_curr, int y_curr, int index);
 #endif
 
