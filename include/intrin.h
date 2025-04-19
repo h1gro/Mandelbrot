cbe64e5a746b6d736cdc6208e6c6898f78aa3278
h1gro <komarov.aa@phystech.edu>
2025-04-20 00:51:08 +0300

    worked intrine

--- include/intrin.h
+++ include/intrin.h
@@ -13,7 +13,8 @@
 
 void FillingIntrin (sf::VertexArray& pixels, struct window_t* win_param);
 void BatchProc(sf::VertexArray& pixels, struct window_t* win_param, __m128 x0, __m128 y0, int x_curr, int y_curr);
-void BatchMandCalc(sf::VertexArray& pixels, struct window_t* win_param, int* id1, __m128 x0, __m128 y0, __m128 x, __m128 y, int x_curr, int y_curr, int calcs);
+
+int BatchMandCalc(sf::VertexArray& pixels, struct window_t* win_param, int* id1, __m128 x0, __m128 y0, __m128 x, __m128 y, int x_curr, int y_curr, int calcs);
 //void CalcsArraysMandelbrotIntrin (sf::VertexArray& pixels, struct window_t* win_param, struct int_arr_t* coords, int x_curr, int y_curr);
 
 //int CurrentCalcsIntrin (struct int_arr_t* coords, int i, int* identificator);
